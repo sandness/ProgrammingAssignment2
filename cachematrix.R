d98780c52d3a7d4b5c70cf5fe5d852f7a7439685
Eric Sandness <ericdsandness000@yahoo.com>
2014-07-20 14:34:20 +0100

    Implemented code for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow you to create an environment containing a matrix
+## and its cached inverse, and evaluate the inverse (making use of the
+## cached value if available) on demand.
 
-## Write a short comment describing this function
+## Creates an environment containing a matrix, and a variable to cache
+## its inverse.  Returns a list object that provides the following methods:
+##   get():  Retrieves the value of the matrix.
+##   getInverse():  Gets the inverse of the matrix.  If a cached value is
+##     not available, the inverse is computed, cached, and returned.
+##     Otherwise, the cached value is returned directly.
+##   set():  Changes the value of the matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {        
+        inverse <- NULL
 
+        get <- function() x
+
+        getInverse <- function(...) {
+                if (is.null(inverse)) inverse <<- solve(x, ...)
+                inverse
+        }
+
+        set <- function(new) {
+                if (!identical(x, new)) {
+                        inverse <<- NULL
+                        x <<- new
+                }
+        }
+
+        list(get = get, getInverse = getInverse, set = set)
 }
 
 
-## Write a short comment describing this function
+## Evalutes the inverse of a matrix that has been wrapped using
+## makeCacheMatrix().  Arguments after the wrapped matrix will be passed
+## through to the solve() function when a matrix inverse is calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        x$getInverse(...)
 }
